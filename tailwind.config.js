52eafcdec20cf7224405fa83ff7a1c13bc4566bb
Dan Alin <alin.ioan.dan@accenture.com>
2023-07-29 00:37:01 +0200

    chore: remove pandscss in favor of tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        //TODO handle theme config properly
+        background: "#09090a",
+      },
+    },
+  },
+  plugins: [],
+};
