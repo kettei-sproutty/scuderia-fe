118229515804321b6343a58893696e4f8a454ce1
Dan Alin <alin.ioan.dan@accenture.com>
2023-07-29 14:17:39 +0200

    chore(tailwind.config): changed from js to ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import { Config } from "tailwindcss";
+
+export default {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
@@ -13,4 +14,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+} satisfies Config;
