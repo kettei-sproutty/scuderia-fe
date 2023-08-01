f50d2f1d2b38195cc878f810adc6877a3bbdca93
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2023-08-01 18:33:43 +0200

    feat: redirect to coming-soon

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import { redirect } from "next/navigation";
+
+const NotFoundPage = () => {
+  redirect("/coming-soong");
+
+  return <></>;
+};
+
+export default NotFoundPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+// import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 const HomePage = async () => {
-  const supabase = createServerComponentClient({ cookies });
-  const { data: userData } = await supabase.auth.getUser();
+  // const supabase = createServerComponentClient({ cookies });
+  // const { data: userData } = await supabase.auth.getUser();
 
-  if (!userData.user) redirect("/auth");
+  redirect("/coming-soon");
 
-  return (
-    <div>
-      <pre>{JSON.stringify(userData.user, undefined, "\t")}</pre>
-    </div>
-  );
+  return <div>{/* <pre>{JSON.stringify(userData.user, undefined, "\t")}</pre> */}</div>;
 };
 
 export default HomePage;
