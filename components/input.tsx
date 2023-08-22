4fc6f203aed64a58c7f701b3a2e23f66a1e4f616
Francesco Malagisi <malagisifra@gmail.com>
2023-08-22 14:59:12 +0200

    feat: add input-text (#44)

--- components/input.tsx
+++ components/input.tsx
@@ -1,28 +1,32 @@
-import type { InputHTMLAttributes } from "react";
+import React from "react";
 
-type InputProps = {
+type InputTextProps = {
   label: string;
-} & InputHTMLAttributes<HTMLInputElement>;
+  error?: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
 
-const Input = ({ label, ...props }: InputProps) => {
-  return (
+const InputText = React.forwardRef<HTMLInputElement, InputTextProps>(
+  ({ label, required, error, id, ...props }, ref) => (
     <div>
-      <label
-        htmlFor={props.id}
-        className={"text-sm font-medium  text-white focus-within:bg-indigo-600"}
-      >
+      <label htmlFor={id} className="text-sm font-medium  text-white focus-within:bg-indigo-600">
         {label}
+        {required && <span>*</span>}
       </label>
-      <div className={"mt-2"}>
+      <div className="mt-2">
         <div className="mt-2 flex items-center rounded-md border border-gray-300 bg-gray-100">
           <input
-            className={`flex w-full  border-0 bg-gray-600 px-1 py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0`}
+            type="text"
+            ref={ref}
+            className="flex w-full  border-0 bg-gray-600 px-1 py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0"
             {...props}
           />
         </div>
       </div>
+      {error && <span>{error}</span>}
     </div>
-  );
-};
+  ),
+);
 
-export default Input;
+InputText.displayName = "InputTextComponent";
+
+export default InputText;
