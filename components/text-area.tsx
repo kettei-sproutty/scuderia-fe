653d8727b3dc24305ae12ec1258933718d77e796
Francesco <malagisifra@gmail.com>
2023-08-23 11:16:12 +0200

    feat: textarea

---
+++ components/text-area.tsx
@@ -0,0 +1,34 @@
+import React, { forwardRef } from "react";
+
+type TextAreaProps = {
+  label: string;
+  error?: string;
+} & React.InputHTMLAttributes<HTMLTextAreaElement>;
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ label, placeholder, required, value, error, id, onChange }, ref) => {
+    return (
+      <div>
+        <label htmlFor={id} className="text-sm font-medium  text-white focus-within:bg-indigo-600">
+          {label}
+          {required && <span>*</span>}
+        </label>
+        <div className="mt-2">
+          <div className="mt-2 flex items-center rounded-md border border-gray-300 bg-gray-100">
+            <textarea
+              className="rounded-lg border border-gray-300 p-2"
+              placeholder={placeholder}
+              value={value}
+              onChange={onChange}
+              ref={ref}
+            />
+          </div>
+        </div>
+        {error && <span>{error}</span>}
+      </div>
+    );
+  },
+);
+
+TextArea.displayName = "TextAreaComponent";
+
+export default TextArea;
