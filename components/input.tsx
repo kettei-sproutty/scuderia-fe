522779ede03db7327f7af41912cef37573904d2e
Francesco <malagisifra@gmail.com>
2023-08-31 10:11:50 +0200

    fix: revert input

--- components/input.tsx
+++ components/input.tsx
@@ -1,58 +1,32 @@
 import { cn } from "@utils/cn";
-import { VariantProps, cva } from "class-variance-authority";
 import React, { ReactNode } from "react";
-
-const containerVariants = cva("flex w-full flex-col", {
-  variants: {
-    size: {
-      sm: "text-xs",
-      md: "text-sm",
-      lg: "text-md",
-    },
-  },
-  defaultVariants: {
-    size: "md",
-  },
-});
-
-const labelInputWrapperVariants = cva(
-  "relative grid w-full grid-cols-[minmax(25%,auto)_1fr] rounded-sm text-primary-100 focus-within:ring-2 focus-within:ring-primary-100 hover:ring-2 hover:ring-primary-100 focus:outline-none",
-  {
-    variants: {
-      error: {
-        default: "border border-primary-100",
-        error: "border-2 border-error-light",
-      },
-      suffix: {
-        default: "grid-cols-[minmax(25%,auto)_1fr]",
-        suffix: "grid-cols-[minmax(25%,auto)_1fr_minmax(25%,auto)]",
-      },
-    },
-    defaultVariants: {
-      error: "default",
-      suffix: "default",
-    },
-  },
-);
 
 type InputTextProps = {
   label: string;
+  error?: string;
   id: string;
+  suffix?: ReactNode;
+  size?: "sm" | "md" | "lg";
   //TODO define if we want variants with icons/prefixes/suffixes
-} & React.InputHTMLAttributes<HTMLInputElement> &
-  VariantProps<typeof containerVariants> &
-  VariantProps<typeof labelInputWrapperVariants>;
+} & React.InputHTMLAttributes<HTMLInputElement>;
 
 const InputText = React.forwardRef<HTMLInputElement, InputTextProps>(
-  ({ label, required, error, id, suffix, size, ...props }, ref) => {
+  ({ label, required, error, id, suffix, size = "md", ...props }, ref) => {
     return (
-      <div className={cn(containerVariants({ size }))}>
+      <div
+        className={cn(
+          "flex w-full flex-col",
+          { "text-xs": size === "sm" },
+          { "text-sm": size === "md" },
+          { "text-md": size === "lg" },
+        )}
+      >
         <div
           className={cn(
-            labelInputWrapperVariants({
-              error: error ? "error" : "default",
-              suffix: suffix ? "suffix" : "default",
-            }),
+            "relative grid w-full grid-cols-[minmax(25%,auto)_1fr] border border-primary-100 rounded-sm text-primary-100 focus-within:ring-2 focus-within:ring-primary-100 hover:ring-2 hover:ring-primary-100 focus:outline-none",
+            { "border-2 border-error-light": error },
+            { "grid-cols-[minmax(25%,auto)_1fr]": !suffix },
+            { "grid-cols-[minmax(25%,auto)_1fr_minmax(25%,auto)]": suffix },
           )}
         >
           <label htmlFor={id} className="input-text-label border-r border-primary-50">
