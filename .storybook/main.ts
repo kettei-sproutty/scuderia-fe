b6dd9e1d6287de07bbfb9bff838652803ad214a5
Francesco Malagisi <malagisifra@gmail.com>
2023-09-13 09:03:16 +0200

    feat: upvote (#104)
    * feat: upvote
    
    * chore: upgrade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
